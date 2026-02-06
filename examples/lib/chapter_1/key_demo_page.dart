73e4b985a6687ad20d5d0f1caaa132ae06747a19
spike <spike@qq.com>
2026-02-06 11:18:20 +0800

    提交key 匹配的demo

--- examples/lib/chapter_1/key_demo_page.dart
+++ examples/lib/chapter_1/key_demo_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class KeyDemoPage extends StatelessWidget {
@@ -9,12 +11,73 @@ class KeyDemoPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Key与复用'),
       ),
-      body: const Center(
-        child: Text(
-          'Key与复用',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+      body: const WrappedStatefulkeyWidget(),
     );
   }
+}
+class WrappedStatefulkeyWidget extends StatefulWidget {
+  const WrappedStatefulkeyWidget({super.key});
+  @override
+  State<StatefulWidget> createState() => _WrappedStatefulkeyState();
+}
+class _WrappedStatefulkeyState extends State<WrappedStatefulkeyWidget> {
+
+  List<Widget> tiles = [
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: StatefulColorfulTile(key:const ValueKey(1)),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: StatefulColorfulTile(key:const ValueKey(2)),
+    ),
+  ];  
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(children: tiles),
+      floatingActionButton: FloatingActionButton(
+          onPressed: swapTiles,
+          child: const Icon(Icons.sentiment_very_satisfied)),
+    );
+  }
+
+  swapTiles() {
+    setState(() {
+      tiles = tiles.reversed.toList();
+    });
+  }
+}
+
+class StatefulColorfulTile extends StatefulWidget {
+  StatefulColorfulTile({Key? key}) : super(key: key);  // NEW CONSTRUCTOR
+
+  @override
+  ColorfulTileState createState() => ColorfulTileState();
+}
+
+class ColorfulTileState extends State<StatefulColorfulTile> {
+  Color? myColor;
+
+  @override
+  void initState() {
+    super.initState();
+    myColor = generateRandomColor();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: myColor,
+        width: 100,height: 100,);
+  }
+  generateRandomColor() {
+    return Color.fromARGB(
+        255,
+        Random().nextInt(255),
+        Random().nextInt(255),
+        Random().nextInt(255));
+}
 }
